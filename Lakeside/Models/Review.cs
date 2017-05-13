9197378d1a1694f95a3d04ef2cab730d405dfa1c
Kevin Jacks <kevinjackswi@gmail.com>
2017-05-13 16:17:16 -0500

    Add member variables to Review class

--- Lakeside/Models/Review.cs
+++ Lakeside/Models/Review.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace Lakeside.Models
 {
     public class Review
     {
+        [Required, Key]
+        public int MemberID { get; set; }
+        [Required, Key]
+        public int FilmID { get; set; }
+        [Required]
+        public DateTime ReviewDate { get; set; }
+        [Required]
+        public int Rating { get; set; }
+        [Required, MaxLength(100)]
+        public string ReviewTitle { get; set; }
+        [Required, MaxLength(1000)]
+        public string FullReview { get; set; }
+
+        // get review single
+
+        // CUD review
     }
 }
