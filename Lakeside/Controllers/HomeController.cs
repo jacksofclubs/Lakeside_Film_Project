42b57b66ccebb2e192e9b76a6b4c14503d4b7c8f
Kevin Jacks <kevinjackswi@gmail.com>
2017-05-13 07:52:14 -0500

    Added FilmListVM and ability to set session variable
    Added provided code to FilmListVM class

--- Lakeside/Controllers/HomeController.cs
+++ Lakeside/Controllers/HomeController.cs
@@ -11,7 +11,17 @@ namespace Lakeside.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            if (Session["memberid"] == null)
+                Session["memberid"] = 1;
+            ViewBag.memberid = Session["memberid"];
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult SetMemberid(int memberid)
+        {
+            Session["memberid"] = memberid;
+            return RedirectToAction("index");
         }
     }
 }
---
+++ Lakeside/Models/ViewModels/FilmListVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lakeside.Models;
+namespace Lakeside.Models.ViewModels
+{
+    public class FilmListVM
+    {
+        public int selectedcatid { get; set; }
+        public List<Film> films { get; set; }
+        public IEnumerable<SelectListItem> catlist { get; set; }
+    }
+}
