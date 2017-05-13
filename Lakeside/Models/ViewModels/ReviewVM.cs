519885da7f5b1eb097c20100beaa0d4ba08346c2
Kevin Jacks <kevinjackswi@gmail.com>
2017-05-13 16:07:42 -0500

    Add provided code for ReviewVM and foundation code for Review class

--- Lakeside/Models/ViewModels/ReviewVM.cs
+++ Lakeside/Models/ViewModels/ReviewVM.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Lakeside.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using System.Web.Mvc;
 namespace Lakeside.Models.ViewModels
 {
     public class ReviewVM
     {
+        public Review review { get; set; }
+        public IEnumerable<SelectListItem> filmlist { get; set; }
+        public int SelectedFilm { get; set; }
     }
 }
